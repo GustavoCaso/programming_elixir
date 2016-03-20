ea04b333d6f80b6a1a05a4499417727f570402d9
GustavoCaso <gustavocaso@gmail.com>
2016-03-20 19:37:48 +0100

    Finish with MyEnum exercises

--- chapter_10/my_enum/lib/my_enum.ex
+++ chapter_10/my_enum/lib/my_enum.ex
@@ -21,4 +21,34 @@ defmodule MyEnum do
     end
     _filter(tail, func, acc)
   end
+
+  def split(list, count), do: _split(list, [], count)
+  defp _split(tail, acc, 0), do: { acc, tail }
+  defp _split([head|tail], acc, count) do
+    acc = acc ++ [head]
+    _split(tail, acc, count - 1)
+  end
+
+  def take(list, count), do: _take(list, [], count)
+  defp _take(_tail, acc, 0), do: acc
+  defp _take(list = [head|tail], acc, count) when count < 0 do
+    acc = acc ++ [_get_last(tail)]
+    new_list = _remove_last(list)
+    _take(new_list, acc, count + 1)
+  end
+  defp _take([head|tail], acc, count) do
+    acc = acc ++ [head]
+    _take(tail, acc, count - 1)
+  end
+
+  defp _get_last([n]), do: n
+  defp _get_last([_|t]), do: _get_last(t)
+
+  defp _remove_last(list) do
+    List.delete(list, _get_last(list))
+  end
+
+  def flatten([head|tail]) when is_list(head), do: flatten(head) ++ flatten(tail)
+  def flatten([head|tail]), do: [head] ++ flatten(tail)
+  def flatten([]), do: []
 end
--- chapter_10/my_enum/test/my_enum_test.exs
+++ chapter_10/my_enum/test/my_enum_test.exs
@@ -25,4 +25,20 @@ defmodule MyEnumTest do
   test "#split will return a tuple with the list divided by the count" do
     assert(MyEnum.split([1, 2, 3], 2) == { [1,2], [3] })
   end
+
+  test "#split will return a tuple with the list divided by the count" do
+    assert(MyEnum.split([1, 2, 3, 4,5,6], 4) == { [1,2,3,4], [5,6] })
+  end
+
+  test "#take will return a list with the total of elements provided by the count argument" do
+    assert(MyEnum.take([1, 2, 3, 4,5,6], 4) == [1,2,3,4])
+  end
+
+  test "#take will return a list from the back of the list if the number is negative" do
+    assert(MyEnum.take([1, 2, 3, 4,5,6], -4) == [6,5,4,3])
+  end
+
+  test '#flatten will return all values from list even if the value is a list it self' do
+    assert(MyEnum.flatten([ 1, [ 2, 3, [4] ], 5, [[[6]]]]) == [1,2,3,4,5,6])
+  end
 end
